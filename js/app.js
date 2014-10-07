65233529e6265784dce23e43803a2d91b71c62d1
Rasmus Cederdorff <rasmus@cederdorff.com>
2014-10-07 09:23:45 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+var ryhave = angular.module('ryhave', ['ngRoute']);
+
+ryhave.config(function($routeProvider, $locationProvider) {
+
+  $routeProvider.when('/', {
+    templateUrl: 'pages/home.html',
+    controller: 'mainController'
+  }).when('/processen', {
+    templateUrl: 'pages/processen.html'
+  }).when('/om', {
+    templateUrl: 'pages/om.html'
+  }).when('/pris', {
+    templateUrl: 'pages/pris.html'
+  }).when('/faq', {
+    templateUrl: 'pages/faq.html'
+  }).when('/:prefix', {
+    templateUrl: 'pages/kontakt.html',
+    controller: 'mainController'
+  }).otherwise({
+    redirectTo: '/'
+  });
+  // $locationProvider.html5Mode(true);
+  $locationProvider.hashPrefix('!');
+});
+
+ryhave.controller('NavCtrl', function($scope, $location) {
+    $scope.isActive = function (viewLocation) { 
+      console.log(viewLocation);
+        return viewLocation === $location.path();
+    };
+  });
+
+ryhave.controller('mainController', function($scope, $location) {
+
+});
