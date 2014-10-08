853894dca0cf834969cccc4393b2cea6dcb259bc
Rasmus Cederdorff <rasmus@cederdorff.com>
2014-10-08 10:05:12 +0200

    Added contact form behavior

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ ryhave.config(function($routeProvider, $locationProvider) {
     templateUrl: 'pages/faq.html'
   }).when('/:prefix', {
     templateUrl: 'pages/kontakt.html',
-    controller: 'mainController'
+    controller: 'contactController'
   }).otherwise({
     redirectTo: '/'
   });
@@ -49,3 +49,38 @@ ryhave.controller('sagsgangController', function($scope, $document){
     }
   }
 ).value('duScrollOffset', 154);
+
+ryhave.controller('contactController', function ($scope, $http) {
+    $scope.result = 'hidden'
+    $scope.resultMessage;
+    $scope.formData; //formData is an object holding the name, email, subject, and message
+    $scope.submitButtonDisabled = false;
+    $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
+    $scope.submit = function(contactform) {
+        $scope.submitted = true;
+        $scope.submitButtonDisabled = true;
+        if (contactform.$valid) {
+            $http({
+                method  : 'POST',
+                url     : 'contact-form.php',
+                data    : $.param($scope.formData),  //param method from jQuery
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+            }).success(function(data){
+                console.log(data);
+                if (data.success) { //success comes from the return json object
+                    $scope.submitButtonDisabled = true;
+                    $scope.resultMessage = data.message;
+                    $scope.result='bg-success';
+                } else {
+                    $scope.submitButtonDisabled = false;
+                    $scope.resultMessage = data.message;
+                    $scope.result='bg-danger';
+                }
+            });
+        } else {
+            $scope.submitButtonDisabled = false;
+            $scope.resultMessage = 'Fejl. Udfyld venligst alle felterne.';
+            $scope.result='bg-danger';
+        }
+    }
+});
